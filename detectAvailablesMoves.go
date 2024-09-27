5f120089474591f804eaf10e997e8e7e11b0bf73
stantowne <41483026+stantowne@users.noreply.github.com>
2024-09-27 16:50:25 -0400

    change veryVerbose to singleGame

--- detectAvailablesMoves.go
+++ detectAvailablesMoves.go
@@ -1,13 +1,13 @@
 package main
 
-func detectAvailableMoves(b board, moveCounter int, veryVerbose bool) []move {
+func detectAvailableMoves(b board, moveCounter int, singleGame bool) []move {
 	var aMoves []move //available Moves
-	aMoves = append(aMoves, detectUpMoves(b, moveCounter, veryVerbose)...)
-	aMoves = append(aMoves, detectAcrossMoves(b, moveCounter, veryVerbose)...)
-	aMoves = append(aMoves, detectMecNotThoughtful(b, moveCounter, veryVerbose)...)
-	aMoves = append(aMoves, detectDownMoves(b, moveCounter, veryVerbose)...)
-	aMoves = append(aMoves, detectPartialColumnMoves(b, moveCounter, veryVerbose)...)
-	aMoves = append(aMoves, detectFlipStockToWaste(b, moveCounter, veryVerbose)...)
-	aMoves = append(aMoves, detectFlipWasteToStock(b, moveCounter, veryVerbose)...)
+	aMoves = append(aMoves, detectUpMoves(b, moveCounter, singleGame)...)
+	aMoves = append(aMoves, detectAcrossMoves(b, moveCounter, singleGame)...)
+	aMoves = append(aMoves, detectMecNotThoughtful(b, moveCounter, singleGame)...)
+	aMoves = append(aMoves, detectDownMoves(b, moveCounter, singleGame)...)
+	aMoves = append(aMoves, detectPartialColumnMoves(b, moveCounter, singleGame)...)
+	aMoves = append(aMoves, detectFlipStockToWaste(b, moveCounter, singleGame)...)
+	aMoves = append(aMoves, detectFlipWasteToStock(b, moveCounter, singleGame)...)
 	return aMoves
 }
