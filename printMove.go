c93b9b331c93c09f41e8d06e6a7e33b8f4d948b6
dan <dan@ntuple.net>
2024-10-17 21:42:18 -0500

    started to write printMoves

--- printMove.go
+++ printMove.go
@@ -10,11 +10,16 @@ func printMove(m move) string {
 		outS += "Move: " + m.cardToMove.pStr() + " To the: " + string(m.cardToMove.suitSymbol()) + "Pile  Priority: " + strconv.Itoa(m.priority)
 	case "moveAceUp", "moveDeuceUp", "move3PlusUp":
 		outS += "Move: " + m.cardToMove.pStr() + " From column: " + strconv.Itoa(m.fromCol) + " To: " + string(m.cardToMove.suitSymbol()) + "Pile  Priority: " + strconv.Itoa(m.priority)
-	case "moveAceDown", "moveDeuceDown", "move3PlusDown":
+	case "moveDown":
 		outS += "Move: " //+ m.cardToMove.pStr() + " From column: " + strconv.Itoa(m.fromCol) + " To: " + string(m.cardToMove.suitSymbol()) + "Pile  Priority: " + strconv.Itoa(m.priority)
 	case "moveEntireColumn":
 		outS += "Move the cards starting with: " + m.MovePortion[0].pStr() + " From column: " + strconv.Itoa(m.fromCol) + " To column: " + strconv.Itoa(m.toCol) + "  Priority: " + strconv.Itoa(m.priority)
-
+	case "movePartialColumn":
+		outS += "Move: "
+	case "flipStockToWaste":
+		outS += "Move: "
+	case "flipWasteToStock":
+		outS += "Move: "
 	}
 
 	return outS
