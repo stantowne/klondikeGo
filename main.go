37e06744a14d5ebe8a2568e609dbdf6bfcfff474
dan <dan@ntuple.net>
2024-11-21 21:14:36 -0600

    Comments only

--- main.go
+++ main.go
@@ -55,9 +55,6 @@ type ConfigurationSubsetForSQLWriting struct { // STAN not sure we even need to 
 }
 
 func main() {
-	// Temp **********************
-	PrintWinningMoves = true
-	// Temp **********************
 
 	// unmarshal YAML file
 	cfg := Configuration{}
