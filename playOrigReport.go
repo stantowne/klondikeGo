98f02c72fcf15814bc809153063afd9228fd6d51
dan <dan@ntuple.net>
2024-12-04 19:45:09 -0600

    changed p.Fprintf( to pfmt.Fprintf( in playOrigReport

--- playOrigReport.go
+++ playOrigReport.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/text/language"
-	"golang.org/x/text/message"
 )
 
 func playOrigReport(vPO variablesSpecificToPlayOrig, cfg *Configuration) {
@@ -11,12 +9,11 @@ func playOrigReport(vPO variablesSpecificToPlayOrig, cfg *Configuration) {
 	lossCounter := cfg.General.NumberOfDecksToBePlayed - vPO.winCounter
 	elapsedTime := vPO.endTime.Sub(vPO.startTime)
 	percentageAttemptsAvoided := 100.0 * float64(vPO.attemptsAvoidedCounter) / float64(possibleAttempts)
-	var p = message.NewPrinter(language.English)
 
 	fmt.Fprintf(oW, "\n\nDate & Time Completed is %v\n", vPO.endTime)
 	var err error
 
-	_, err = p.Fprintf(oW, "Number of Decks Played is %d, starting with Deck %d.\n", cfg.General.NumberOfDecksToBePlayed, cfg.General.FirstDeckNum)
+	_, err = pfmt.Fprintf(oW, "Number of Decks Played is %d, starting with Deck %d.\n", cfg.General.NumberOfDecksToBePlayed, cfg.General.FirstDeckNum)
 	if err != nil {
 		fmt.Println("Number of Decks Played cannot print")
 	}
@@ -25,7 +22,7 @@ func playOrigReport(vPO variablesSpecificToPlayOrig, cfg *Configuration) {
 
 	fmt.Fprintf(oW, "Number of Initial Override Strategies Per Deck is %d.\n", vPO.numberOfStrategies)
 
-	_, err = p.Fprintf(oW, "Number of Possible Attempts is %d.\n", possibleAttempts)
+	_, err = pfmt.Fprintf(oW, "Number of Possible Attempts is %d.\n", possibleAttempts)
 	if err != nil {
 		fmt.Println("Number of Possible Attempts cannot print")
 	}
@@ -34,37 +31,37 @@ func playOrigReport(vPO variablesSpecificToPlayOrig, cfg *Configuration) {
 
 	fmt.Fprintf(oW, "Elapsed Time is %v.\n", elapsedTime)
 
-	_, err = p.Fprintf(oW, "Total Decks Won is %d of which %d were Early Wins\n", vPO.winCounter, vPO.earlyWinCounter)
+	_, err = pfmt.Fprintf(oW, "Total Decks Won is %d of which %d were Early Wins\n", vPO.winCounter, vPO.earlyWinCounter)
 	if err != nil {
 		fmt.Println("Total Decks Won cannot print")
 	}
 
-	_, err = p.Fprintf(oW, "Total Decks Lost is %d\n", lossCounter)
+	_, err = pfmt.Fprintf(oW, "Total Decks Lost is %d\n", lossCounter)
 	if err != nil {
 		fmt.Println("Total Decks Lost cannot print")
 	}
 
-	_, err = p.Fprintf(oW, "Losses at Game Length Limit is %d\n", vPO.lossesAtGLL)
+	_, err = pfmt.Fprintf(oW, "Losses at Game Length Limit is %d\n", vPO.lossesAtGLL)
 	if err != nil {
 		fmt.Println("Losses at Game Length Limit cannot print")
 	}
 
-	_, err = p.Fprintf(oW, "Losses at No Moves Available is %d\n", vPO.lossesAtNoMoves)
+	_, err = pfmt.Fprintf(oW, "Losses at No Moves Available is %d\n", vPO.lossesAtNoMoves)
 	if err != nil {
 		fmt.Println("Losses at No Moves Available cannot print")
 	}
 
-	_, err = p.Fprintf(oW, "Regular Losses is %d\n", vPO.regularLosses)
+	_, err = pfmt.Fprintf(oW, "Regular Losses is %d\n", vPO.regularLosses)
 	if err != nil {
 		fmt.Println("Regular Losses cannot print")
 	}
 
-	_, err = p.Fprintf(oW, "Number of Attempts Avoided ia %d\n", vPO.attemptsAvoidedCounter)
+	_, err = pfmt.Fprintf(oW, "Number of Attempts Avoided ia %d\n", vPO.attemptsAvoidedCounter)
 	if err != nil {
 		fmt.Println("Number of Attempts Avoided cannot print")
 	}
 
-	_, err = p.Fprintf(oW, "Percentage of Possible Attempts Avoided is %v\n", percentageAttemptsAvoided)
+	_, err = pfmt.Fprintf(oW, "Percentage of Possible Attempts Avoided is %v\n", percentageAttemptsAvoided)
 	if err != nil {
 		fmt.Println("Percentage of Possible Attempts Avoided cannot print")
 	}
