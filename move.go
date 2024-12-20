457392710762b4eda1b45bce0d8ed4169974f7e5
dan <dan@ntuple.net>
2024-12-20 14:37:41 -0600

    move.go comments regarding the slice MovePortionStartIdx have been added.

--- move.go
+++ move.go
@@ -7,8 +7,12 @@ type move struct {
 	toCol               int
 	fromCol             int
 	MovePortionStartIdx int
-	MovePortion         []Card //used in mec, mpc,
+	MovePortion         []Card //used in mec, mpc,                          // See comments below
 	cardToMove          Card   //used in Up, Down, Across and mpc
 	colCardFlip         bool   //does the move result in a column Card flip
 
+	// When winning moves are saved to SQL, the slice MovePortion is NOT written.
+	// If and when a routine to read And EXECUTE the winning moves is written the
+	// slice MUST be written by reading the then current board from MovePortionStartIdx
+	// on down.
 }
